e4da148d4465a9f3d19b4db024abb67d7c0b0d13
Robinson Betancur Marin <robinsonb@iadb.org>
2023-01-06 18:08:16 -0500

    start list category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  const onAddCategory = () => {
+    // categories.push('Valorant');
+    setCategories(['Valorant', ...categories]);
+    // setCategories((cat) => [...cat, 'Valorant']);
+  };
+
   return (
     <>
+      {/* Titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gif Cards */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+      {/* Git Item */}
     </>
   );
 };
